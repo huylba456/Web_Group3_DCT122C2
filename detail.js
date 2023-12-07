80b2276dda39d93e9f1062d87a787d024527a3d8
Dang Minh Hao <nhi.hk07597@sinhvien.hoasen.edu.vn>
2023-12-08 01:09:48 +0700

    new

--- detail.js
+++ detail.js
@@ -1,14 +1,4 @@
-document.querySelector('.product__amount--minus').addEventListener('click', () => {
-    var value=document.querySelector('.product__amount--number').value;
-    value--;
-    if(value<1) value=1;
-    document.querySelector('.product__amount--box input').value=value;
-});
- document.querySelector('.product__amount--add').addEventListener('click', () => {
-    var value=document.querySelector('.product__amount--number').value;
-    value++;
-    document.querySelector('.product__amount--box input').value=value;
- });
+
 document.querySelector('.product__addtocart--btn').addEventListener('click', () => {
     alert('Thêm vào giỏ hàng thành công!');
 });
--- order.js
+++ order.js
@@ -1,18 +1,7 @@
 
-document.querySelector('.check-shipping-info').addEventListener('click', () => {
-    document.querySelector('.modal').style.display='block';
-    document.querySelector('.activemauden').style.display = 'block';
-});
-document.querySelector('.close').addEventListener('click', () => {
-    document.querySelector('.modal').style.display='none';
-    document.querySelector('.activemauden').style.display = 'none';
-});
 document.querySelector('.save').addEventListener('click', () => {
     alert('Lưu thông tin thành công');
-    document.querySelector('.modal').style.display='none';
-    document.querySelector('.activemauden').style.display = 'none';
 });
 document.querySelector('.checkout').addEventListener('click', () => {
     alert('Thanh toán đơn đặt hàng thành công');
-    location.href = 'index.html';
 });
