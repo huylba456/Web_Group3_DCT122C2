6de5a2612a078169853720da260b008ad6625032
Dang Minh Hao <nhi.hk07597@sinhvien.hoasen.edu.vn>
2023-12-08 09:55:14 +0700

    new

---
+++ dangky.js
@@ -0,0 +1,7 @@
+document.querySelector('.signup-button').addEventListener('click',function(){
+    alert('.Đăng ký thành công ');
+    
+})
+function returnn(){
+    location.href='indexdangnhap.html';
+}
