756268306f67f353e98089cef62e3c732fe89906
Dang Minh Hao <nhi.hk07597@sinhvien.hoasen.edu.vn>
2024-05-09 00:22:34 +0700

    new

--- D/backup/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Nhúng file định nghĩa controller vào để có thể dùng được class định nghĩa trong file đó
-include_once('controller/' . $controller . '.php');
-// Tạo ra tên controller class từ các giá trị lấy được từ URL sau đó gọi ra để hiển thị trả về cho người dùng.
-
-$controller = new $controller();
-$controller->$action();
